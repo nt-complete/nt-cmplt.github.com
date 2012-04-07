d349c425f8d4decfcbc382b285a2dae812c3bf3c
Nick Tiller <nicholas@uncorkedstudios.com>
2012-04-06 23:42:01 -0700

    added dirs

---
+++ script/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+	$("#work a").css("color", "#33C");
+
+	var onButton = 1;
+	
+	var workButton = $("#work");
+	var personalButton = $("#personal");
+	
+	
+	
+	workButton.click(function(e) {
+		e.preventDefault();
+		
+		if(onButton == 0) {
+			$("#personal a").css("color", "#505050");
+			$("#work a").css("color", "#33C");
+			
+			$("#personalDiv").animate({height: 'toggle'}, 1000, function() {
+				$("#workDiv").animate({height: 'toggle'}, 1000);
+			});
+			onButton = 1;
+		}
+	});
+	
+	$("#header .button").mouseover(function() {
+		$(this).css("text-decoration", "underline");
+	}).mouseout(function() {
+		$(this).css("text-decoration", "none");
+	});
+	
+	
+	
+	personalButton.click( function(e) {
+		e.preventDefault();
+		
+		if(onButton == 1) {
+			$("#personal a").css("color", "#33C");
+			$("#work a").css("color", "#505050");
+			
+			$("#workDiv").animate({height: 'toggle'}, 1000, function() {
+				$("#personalDiv").animate({height: 'toggle'}, 1000);
+			});
+			onButton = 0;
+		}
+	});
+	
+	$("#infoDiv").click(function() {
+	});
+});
